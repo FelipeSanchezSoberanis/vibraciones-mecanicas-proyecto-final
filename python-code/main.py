b36c9833029e8638d08304dc3d5979a708a4d11a
Felipe  Sánchez <fs@wayakna.com>
2022-05-19 18:56:52 -0500

    Removed comments

--- python-code/main.py
+++ python-code/main.py
@@ -39,33 +39,6 @@ def animate(_):
 
         pass
 
-    #  x = []
-    #  y = []
-
-    #  start_time = time.time()
-
-    #  while True:
-
-    #  line_as_bytes = ser.readline()
-    #  line_as_string = bytes.decode(line_as_bytes)
-    #  try:
-    #  distance = int(line_as_string)
-
-    #  current_time = time.time()
-
-    #  x.append(current_time - start_time)
-
-    #  y.append(distance)
-    #  except ValueError:
-    #  continue
-
-    #  print("Tiempo: {}, distancia: {}".format(current_time - start_time, distance))
-
-    #  ser.flushOutput()
-    #  ser.flushInput()
-
-    #  time.sleep(1)
-
 
 ani = animation.FuncAnimation(fig, animate, interval=1)
 plt.show()
