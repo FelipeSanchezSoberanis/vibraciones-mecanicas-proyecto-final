22ee49db96d0a4a329c56e8bcc2f668f46651cd4
Felipe  Sánchez <fs@wayakna.com>
2022-05-19 19:38:41 -0500

    Fixed compile script

--- arduino-code/compile_upload.sh
+++ arduino-code/compile_upload.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-sketch_file=sketch_may17a
+sketch_file=arduino-code.ino
 
 arduino-cli compile --fqbn arduino:avr:uno ${sketch_file}
 arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:uno ${sketch_file}
